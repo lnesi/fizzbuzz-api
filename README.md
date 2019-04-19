9110c38b36d9da37b5c5fe873c45b69eb0452e32
Luis Nesi <lenesi@hotmail.com>
2019-04-19 15:29:57 +0100

    docs and add cookie session

--- README.md
+++ README.md
@@ -12,13 +12,36 @@ npm run dev
 
 In both cases once start or development the API will become available at ```http://localhost:3000```
 
-## End Points
+## End Points (API Specs)
 
 ```
-GET: /api/v1/fizzbuzz
+GET: /api/v1/fizzbuzz?page={int=1}&size{int=100}
 ```
 Will return the numerical series list where  the value is divisible by 3 is Fizz, divisible by 5 is Buzz, divisible by 3 and 5 is FizzBuzz if not then the number.
 
-#### Parameters (QueriSyring)
+#### Parameters (QuerySyring)
 
-|--Name--|----Description----|
+|Name    | Type  | Description    |
+|--------|-------|----------------|
+|page    |int  | Current page default 1 if negative or 0 will default in 1 |
+|size    |int   | Items per page or page size default 100 if negative or 0 default to 100 if mayor than list scope (100,000,000,000) default to scope |
+
+#### Return Object (JSON)
+|Name    | Type  | Description    |
+|--------|-------|----------------|
+|page    | PageObject| Pagination Object |
+|data    | Array[FizzBuzzObject]| Fizzbuzz response object |
+
+**PageObject**
+|Name    | Type  | Description    |
+|--------|-------|----------------|
+|current | int   | Current page index starts at 1 |
+|size    | int   | Items per page |  
+|total    | int| Total page available scope devided by size |
+
+**FizzBuzzObject**
+|Name    | Type  | Description    |
+|--------|-------|----------------|
+|id      | int   | Id or index of item on list 0>=id<=(FIZZ_BUZZ_SCOPE=100,000,000,000) |
+|value    | mixed   | Integer or String constant for Fizz, Buzz and FizzBuzz |  
+|fav   | boolean| Current Session favorite  |
--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
 const FizzBuzz = require("./FizzBuzz");
 
 const app = express();
+app.set('trust proxy', 1)
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_KEY || "this-is-random-hash"],
+    maxAge: process.env.COOKIE_MAX_AGE || 60 * 60 * 1000 //1 hour
+  })
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Fizz Buzz API!"));
+
 app.get("/api/v1/fizzbuzz", (req, res) => {
+  if(!req.session.favorites){
+    req.session.favorites=[]
+  }
+
   // Getting Pagination Parameters
   // Web can do error handling here instead of condition with parseInt
   // or forcing unallowed values to the default.
   let currentPage = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
   // Reset to 1 un allowed negative and 0;
-  if(currentPage<=0)currentPage=1;
+  if (currentPage <= 0) currentPage = 1;
   let pageSize = parseInt(req.query.size) ? parseInt(req.query.size) : 100;
   //Constrain Page Size
-  if(pageSize>FizzBuzz.LIST_SCOPE )pageSize=FizzBuzz.LIST_SCOPE;
-  let totalPages=Math.ceil(FizzBuzz.LIST_SCOPE / pageSize);
+  if (pageSize > FizzBuzz.LIST_SCOPE) pageSize = FizzBuzz.LIST_SCOPE;
+  let totalPages = Math.ceil(FizzBuzz.LIST_SCOPE / pageSize);
   //Constain current page
-  if(currentPage>totalPages) currentPage = totalPages;
+  if (currentPage > totalPages) currentPage = totalPages;
 
-  const list = FizzBuzz.getList(currentPage-1, pageSize);
+  const list = FizzBuzz.getList(currentPage - 1, pageSize);
   res.send({
     page: {
       current: currentPage,
